b0a31e5edf2dcf6fd6a139b30d5714d9b69cf5df
c <work@hebb.dev>
2022-02-08 20:29:32 -0600

    test

---
+++ app/src/main/java/com/example/colo/LogIn.java
@@ -0,0 +1,94 @@
+package com.example.colo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class LogIn extends AppCompatActivity
+{
+    EditText Email, Password;
+    Button Login;
+
+    //Firebase
+    private FirebaseDatabase database;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private static final String EMPLOYEE = "Employee";
+    private static final String TAG = "CreateAccount";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        Email = (EditText) findViewById(R.id.etEmail);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+        database = FirebaseDatabase.getInstance();
+        mDatabase = database.getReference(EMPLOYEE);
+        mAuth = FirebaseAuth.getInstance();
+
+        Login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                logIn();
+            }
+        });
+    }
+
+    private void logIn()
+    {
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+
+        if(email.isEmpty())
+        {
+            Email.setError("Email is required");
+            Email.requestFocus();
+            return;
+        }
+        if(password.isEmpty())
+        {
+            Password.setError("Password is required");
+            Password.requestFocus();
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if(task.isSuccessful())
+                {
+                    startActivity(new Intent(LogIn.this, ManagerHub.class));
+                }else{
+                    Toast.makeText(LogIn.this, "Failed to login. Please check credentials", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+}
