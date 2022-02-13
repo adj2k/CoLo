1e873affdc4e92f740979e2476883e083b32ebc6
B-Norm <bkbrad1220@gmail.com>
2022-02-13 12:26:18 -0600

    Added EmployeeHub and ClockIn Activity.

---
+++ app/src/main/java/com/example/colo/employeePages/ClockIn.java
@@ -0,0 +1,16 @@
+package com.example.colo.employeePages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.colo.R;
+
+public class ClockIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_in);
+    }
+}
---
+++ app/src/main/java/com/example/colo/employeePages/EmployeeHub.java
@@ -0,0 +1,16 @@
+package com.example.colo.employeePages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.colo.R;
+
+public class EmployeeHub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_hub);
+    }
+}
