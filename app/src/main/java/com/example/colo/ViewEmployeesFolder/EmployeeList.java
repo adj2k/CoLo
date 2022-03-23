0b57d970568de9b3849f708f49169f5b243700c6
c <work@hebb.dev>
2022-03-23 17:39:36 -0500

    Manager: View Employees Page done. TODO: make it so when u click edit, takes u to an edit page, delete-> popup to confirm deletion, and finalize the create employee page then copy/paste all that for manager

---
+++ app/src/main/java/com/example/colo/ViewEmployeesFolder/EmployeeList.java
@@ -0,0 +1,12 @@
+package com.example.colo.ViewEmployeesFolder;
+
+public class EmployeeList {
+
+    String name, employeeID, email;
+
+    public String getName() { return name; }
+    public String getEmployeeID() { return employeeID; }
+    public String getEmail() { return email; }
+
+
+}
