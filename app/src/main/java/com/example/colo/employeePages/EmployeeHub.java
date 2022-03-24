3b820ca6e9d723104d1b9654797c03ade19cc9ca
Nirdesh_Sakh <nirdesh.sakh77@gmail.com>
2022-03-24 01:53:40 -0500

    Updated Employees XML Logout btn on Employee page Small change to ClockInOut xml

--- app/src/main/java/com/example/colo/employeePages/EmployeeHub.java
+++ app/src/main/java/com/example/colo/employeePages/EmployeeHub.java
@@ -2,6 +2,7 @@ package com.example.colo.employeePages;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Build;
@@ -9,9 +10,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.colo.Announcements.CreateAnnouncement;
+import com.example.colo.LogIn;
 import com.example.colo.MainActivity;
 import com.example.colo.ManagerHub;
 import com.example.colo.R;
@@ -20,8 +24,9 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class EmployeeHub extends AppCompatActivity {
 
-    ImageButton clock_in_out;
-    Button  emp_logout, e_ann_btn;
+    LinearLayout clock_in_out,e_projects_btn,e_settings_btn;
+    ConstraintLayout e_ann_btn;
+    ImageView emp_logout;
     private FirebaseAuth eAuth;
 
 
@@ -30,10 +35,10 @@ public class EmployeeHub extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_hub);
 
-        clock_in_out = (ImageButton) findViewById(R.id.clock_in_out_btn);
-        emp_logout = (Button) findViewById(R.id.emp_logout_btn);
+        clock_in_out = findViewById(R.id.clock_in_out_btn);
+        emp_logout = findViewById(R.id.e_logout_btn);
         eAuth = FirebaseAuth.getInstance();
-        e_ann_btn = (Button) findViewById(R.id.e_ann_btn);
+        e_ann_btn = findViewById(R.id.e_ann_btn);
 
         clock_in_out.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,7 +60,7 @@ public class EmployeeHub extends AppCompatActivity {
             public void onClick(View v)
             {
                 eAuth.signOut();
-                startActivity(new Intent(EmployeeHub.this, MainActivity.class));
+                startActivity(new Intent(EmployeeHub.this, LogIn.class));
             }
         });
     }
