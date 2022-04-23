ac611c28a8ebc4343e7580b33e7cd468f2d3f63d
toidle <binhboy2000@gmail.com>
2022-04-23 09:49:03 -0500

    Add manager and add employees fixed.

--- app/src/main/java/com/example/colo/CreateEmployee.java
+++ app/src/main/java/com/example/colo/CreateEmployee.java
@@ -154,6 +154,7 @@ public class CreateEmployee extends AppCompatActivity
                                         // Sign in success, update UI with the signed-in user's information
                                         Log.d(TAG, "signInWithEmail:success");
 
+
                                     } else {
                                         // If sign in fails, display a message to the user.
                                         Log.w(TAG, "signInWithEmail:failure", task.getException());
